a6fa1fe87688f883766459e7b379b1c9ef9b5a71
Brian Toon <brian.elliot.toon@gmail.com>
2025-04-23 15:15:19 -0600

    random word & initial state

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,12 @@
+import { useState } from "react";
+import words from "./words.json";
+
 function App() {
+  const [word, setWord] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)]
+  })
+
+  console.log(word)
 
   return (
     <h1>Hangman</h1>
